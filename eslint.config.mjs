e84ed668168e7124147effb1e77d9d10853f96a8
kensiiwasaki <kensiiwasaki@gmail.com>
2024-01-30 23:15:23 +0900

    設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,38 @@
+// eslint.config.js
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tsEsLintPlugin from "@typescript-eslint/eslint-plugin";
+import tsEsLintParser from "@typescript-eslint/parser";
+
 export default [
+  // 無視するファイルを指定（従来の .eslintignore に相当）
+  { ignores: ["dist"] },
+  // eslint:recommendedに相当
+  js.configs.recommended,
+  // eslint-config-prettierはrulesを持つオブジェクトなので、ここに並べられる
+  eslintConfigPrettier,
+  // プラグインを登録
   {
-    // extends: "next/core-web-vitals",
+    plugins: {
+      "@typescript-eslint": tsEsLintPlugin,
+    },
+  },
+  // TypeScript用の設定
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.cts", "**/*.mts"],
+    languageOptions: {
+      parser: tsEsLintParser,
+      parserOptions: {
+        project: true,
+      },
+    },
     rules: {
-      quotes: ["error", "double"],
-      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      // @typescript-eslint/eslint-pluginに付属のルールを適用
+      ...tsEsLintPlugin.configs["eslint-recommended"].overrides[0].rules,
+      ...tsEsLintPlugin.configs["recommended-type-checked"].rules,
+      // 追加の設定
+      "@typescript-eslint/no-explicit-any": "error",
     },
   },
 ];
